361a6fa1eb714aeb6ed1bfaff424f6d81bf9ec8f
dsapozhnikov <31924660+dsapozhnikov@users.noreply.github.com>
2017-10-31 16:44:04 +0300

    изменнены классы abstractnuit and intputhandler

--- core/src/com/mygdx/game/AbstractUnit.java
+++ core/src/com/mygdx/game/AbstractUnit.java
@@ -1,7 +1,57 @@
 package com.mygdx.game;
 
-/**
- * Created by DSapozhnikov on 31.10.2017.
- */
-public class AbstractUnit {
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class AbstractUnit {
+   protected Texture texture;
+    protected int health;
+    protected String name;
+    protected int maxHealth;
+    protected int level;
+    protected Rectangle rect;
+//primary stats
+    protected int strenght;
+    protected int agility;
+    protected int endurance;
+    protected int intilligence;
+
+    // secondary stats
+
+    protected int defense;
+
+
+
+    protected Vector2 position;
+    protected boolean flip;
+    protected float
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture,position.x,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,0,0,0,texture.getWidth(),texture.getHeight(),flip,false
+
+        );
+
+    public Rectangle getRect() {
+        return rect;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+        this.rect = new Rectangle(position.x,position.y,texture.getWidth(),texture.getHeight());
+    }
+    public  void takeDamage (int damage) {
+        health -= damage;
+    }
+    public void meleeAttack(AbstractUnit enemy) {
+        int dmg  =this.strenght-enemy.defense;
+        if (dmg <0) {
+            dmg   =0;
+        }
+        enemy.takeDamage(dmg);
+
+    }
+
 }
--- core/src/com/mygdx/game/InputHandler.java
+++ core/src/com/mygdx/game/InputHandler.java
@@ -1,7 +1,31 @@
 package com.mygdx.game;
 
-/**
- * Created by DSapozhnikov on 31.10.2017.
- */
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.awt.*;
+
 public class InputHandler {
+
+    public static float getX() {
+        return Gdx.input.getX();
+    }
+
+    public static float getY() {
+        return 720 - Gdx.input.getY();
+    }
+
+    public static boolean checkClickInRect(Rectangle rect) {
+        if (Gdx.input.justTouched()) {
+            if (rect.contains(getX(), getY())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+
