1c4cf19a4e64cc8531b88bb147e1b1e0c303ed44
dsapozhnikov <31924660+dsapozhnikov@users.noreply.github.com>
2017-11-01 19:57:01 +0300

    попытка создать список персов!

--- core/src/com/mygdx/game/AbstractUnit.java
+++ core/src/com/mygdx/game/AbstractUnit.java
@@ -9,8 +9,8 @@ import com.badlogic.gdx.math.Vector2;
 
 import javax.xml.soap.Text;
 
-public abstract class AbstractUnit extends HpLevelBar{
-   protected Texture texture;
+public abstract class AbstractUnit extends HpLevelBar {
+    protected Texture texture;
 
 
     protected int health;
@@ -18,7 +18,7 @@ public abstract class AbstractUnit extends HpLevelBar{
     protected int maxHealth;
     protected int level;
     protected Rectangle rect;
-//primary stats
+    //primary stats
     protected int strenght;
     protected int agility;
     protected int endurance;
@@ -29,7 +29,6 @@ public abstract class AbstractUnit extends HpLevelBar{
     protected int defense;
 
 
-
     protected Vector2 position;
     protected boolean flip;
     protected float attackAction;
@@ -38,28 +37,30 @@ public abstract class AbstractUnit extends HpLevelBar{
 
     public void render(SpriteBatch batch) {
 
-        if (takeDamageAction>0) {
-            batch.setColor(1f,1f-takeDamageAction,1f-takeDamageAction,1f);
+        if (takeDamageAction > 0) {
+            batch.setColor(1f, 1f - takeDamageAction, 1f - takeDamageAction, 1f);
 
         }
-        float dx = (200f*(float) Math.sin((1f - attackAction)* 3.14f));
-        if (flip)dx*= -1;
+        float dx = (200f * (float) Math.sin((1f - attackAction) * 3.14f));
+        if (flip) dx *= -1;
 
+        // batch.draw(texture,position.x+dx,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,0,0,0,texture.getWidth(),texture.getHeight(),flip,false
+        //             );
+        batch.draw(hpFrame, position.x + dx, position.y + 175, 0, 0, 104, 24);
+        batch.draw(hpLength, position.x + 2f + dx, position.y + 176, 0, 0, 101, 22, (float) health / maxHealth, 1, 0, 0, 0, 104, 22, false, false);
 
-       // batch.draw(texture,position.x+dx,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,0,0,0,texture.getWidth(),texture.getHeight(),flip,false
-     //             );
-        batch.draw(hpFrame,position.x+dx,position.y+175,0,0,104,24);
-        batch.draw(hpLength,position.x+2f+dx,position.y+176,0,0,101,22,(float)health / maxHealth,1,0,0,0,104,22,false,false);
-
-        batch.setColor(1f,1f,1f,1f);
-        if (health!=0) {
-            batch.draw(texture,position.x+dx,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,0,0,0,texture.getWidth(),texture.getHeight(),flip,false
+        batch.setColor(1f, 1f, 1f, 1f);
+        if (health != 0) {
+            batch.draw(texture, position.x + dx, position.y, 0, 0, texture.getWidth(), texture.getHeight(), 1, 1, 0, 0, 0, texture.getWidth(), texture.getHeight(), flip, false
             );
 
-        }else {
-            batch.draw(texture,position.x+dx,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,230,0,0,texture.getWidth(),texture.getHeight(),flip,false
+        } else {
+            batch.draw(texture, position.x + dx, position.y, 42, 45, texture.getWidth(), texture.getHeight(), 1, 1, 250, 0, 0, texture.getWidth(), texture.getHeight(), flip, false
             );
-            hpFrame. // killing monstesrrs!
+           // batch.draw(hpFrame, position.x + dx, position.y + 175, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, false, false);
+            // killing monstesrs!
+
+            batch.setColor(0,0,0,0);
 
         }
 
@@ -71,14 +72,15 @@ public abstract class AbstractUnit extends HpLevelBar{
     public Rectangle getRect() {
         return rect;
     }
+
     public void update(float dt) {
 
 
-        if (takeDamageAction>0) {
-            takeDamageAction -= 3*dt;
+        if (takeDamageAction > 0) {
+            takeDamageAction -= 3 * dt;
         }
-        if (attackAction>0) {
-            attackAction -= (float)3*dt;
+        if (attackAction > 0) {
+            attackAction -= (float) 3 * dt;
 
         }
 
@@ -87,26 +89,27 @@ public abstract class AbstractUnit extends HpLevelBar{
 
     public void setPosition(Vector2 position) {
         this.position = position;
-        this.rect = new Rectangle(position.x,position.y,texture.getWidth(),texture.getHeight());
+        this.rect = new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
     }
-    public  void takeDamage (int damage) {
-        this.takeDamageAction=1.0f;
-        health -= damage;
-        if (health-damage<0) {
-            health=0;
-        }
-        System.out.println("damage "+damage);
-        System.out.println("health "+health);
-    }
-    public void meleeAttack(AbstractUnit enemy) {
-        this.attackAction=1.0f;
 
-        int dmg  =this.strenght-enemy.defense;
-        if (dmg <0) {
-            dmg   =0;
+    public void takeDamage(int damage) {
+        this.takeDamageAction = 1.0f;
+        health -= damage;
+        if (health - damage < 0) {
+            health = 0;
+        }
+        System.out.println("damage " + damage);
+        System.out.println("health " + health);
+    }
+
+    public void meleeAttack(AbstractUnit enemy) {
+        this.attackAction = 1.0f;
+
+        int dmg = this.strenght - enemy.defense;
+        if (dmg < 0) {
+            dmg = 0;
         }
         enemy.takeDamage(dmg);
 
     }
-
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,24 +7,43 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.awt.event.HierarchyEvent;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Background background;
-	Hero hero;
-	Monster monster;
+	//AbstractUnit unit;
+	//Hero hero;
+	//Monster monster;
+	List<AbstractUnit>units = new LinkedList<AbstractUnit>();
 	AbstractUnit currentUnit;
+	private int heroIndex;
+	private int monsterIndex;
+	AbstractUnit a;
 	
 	@Override
 	public void create () {
+
 		batch = new SpriteBatch();
 		background = new Background();
-		hero = new Hero();
-		hero.setPosition(new Vector2(400,200));
-		monster = new Monster();
-		monster.setPosition(new Vector2(700,200));
-		currentUnit=hero;
+	//	hero = new Hero();
+		
+
+	//	monster = new Monster();
+		units.add(new Monster());
+		units.add(new Hero());
+		units.add(new Monster());
+		//hero.setPosition(new Vector2(400,200));
+		//monster.setPosition(new Vector2(700,200));
+		for (AbstractUnit a: units) {
+			if (a instanceof Hero) {
+				currentUnit=a;
+			}
+		}
+		
 
 	}
 
@@ -37,23 +56,47 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		background.render(batch);
-		hero.render(batch);
-		monster.render(batch);
+		for (AbstractUnit a: units) {
+			a.render(batch);
+		//			monster.render(batch);
+		}
+		
 		batch.end();
 	}
 	public void update(float dt) {
-		monster.update(dt);
-		hero.update(dt);
-		if (currentUnit==hero) {
-			if (InputHandler.checkClickInRect(monster.rect)) {
-				hero.meleeAttack(monster);
-				currentUnit=monster;
+		for (AbstractUnit a : units) {
+			a.update(dt);
+		}
+		for (int i = 0; i <units.size(); i++) {
+			if (currentUnit instanceof Hero) {
+				a = currentUnit;
+				if (units.get(i)instanceof Monster) {
+					if (InputHandler.checkClickInRect(units.get(i).rect)) {
+						currentUnit.meleeAttack(units.get(i));
+						currentUnit=units.get(i);
+					}
+				}
+
+			}if (InputHandler.checkClickInRect(a.rect)) {
+				currentUnit.meleeAttack(a);
+				currentUnit=a;
 			}
+
+
 		}
-		if (InputHandler.checkClickInRect(hero.rect)) {
-			monster.meleeAttack(hero);
-			currentUnit=hero;
-		}
+		
+//		monster.update(dt);
+//		hero.update(dt);
+//		if (currentUnit==hero) {
+//			if (InputHandler.checkClickInRect(monster.rect)) {
+//				hero.meleeAttack(monster);
+//				currentUnit=monster;
+//			}
+//		}
+//		if (InputHandler.checkClickInRect(hero.rect)) {
+//			monster.meleeAttack(hero);
+//			currentUnit=hero;
+//		}
 
 	}
 	
