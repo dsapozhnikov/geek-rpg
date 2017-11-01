89e0d0d80ff8627a14bd47750444d5286ab9cb12
dsapozhnikov <31924660+dsapozhnikov@users.noreply.github.com>
2017-11-01 14:53:00 +0300

    добавлены параметры индикатр жизней!

--- core/src/com/mygdx/game/Monster.java
+++ core/src/com/mygdx/game/Monster.java
@@ -11,21 +11,25 @@ public class Monster extends AbstractUnit {
     public Monster() {
         this.texture = new Texture("charSkeleton.tga");
         this.name = "Skelet";
-        this.maxHealth= 10;
+        this.maxHealth= 50;
         this.health = this.maxHealth;
-        this.level  =1;
-        this.strenght = 5;
+        this.level = 1;
+        this.strenght = 15;
         this.agility = 5;
         this.endurance = 5;
         this.intilligence = 0;
         this.defense = 1;
         this.flip = true;
+        this.hpFrame = new Texture("frame.png");
+        this.hpLength = new Texture("hpLength.png");
+
 
     }
-
-    public void render(SpriteBatch batch) {
-        batch.draw(texture,position.x,position.y,0,0,texture.getWidth(),texture.getHeight(),1,1,0,0,0,texture.getWidth(),texture.getHeight(),true,false
-
-        );
-    }
+//    public void render(SpriteBatch batch) {
+//        batch.draw(hpFrame,position.x,position.y+175,0,0,104,24);
+//        batch.draw(hpLength,position.x,position.y+175,0,0,102,22);
+//    }
 }
+
+
+
