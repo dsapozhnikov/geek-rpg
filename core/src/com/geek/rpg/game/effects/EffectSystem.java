6e295c738bddcb3a22ffe5a11c8a28cc1a90a214
dsapozhnikov <31924660+dsapozhnikov@users.noreply.github.com>
2017-11-09 15:15:56 +0300

    еще изменения

--- core/src/com/geek/rpg/game/effects/EffectSystem.java
+++ core/src/com/geek/rpg/game/effects/EffectSystem.java
@@ -1,7 +1,36 @@
 package com.geek.rpg.game.effects;
 
-/**
- * Created by DSapozhnikov on 09.11.2017.
- */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EffectSystem {
-}
+   private  List<Effect>effectsList;
+   public String name;
+    public EffectSystem(String name) {
+      effectsList =  new ArrayList<Effect>();
+      this.name=name;
+    }
+    public void addEffect(Effect effect) {
+               effectsList.add(effect);
+        }
+        public  void getTurn() {
+            for (int i = 0; i < effectsList.size(); i++) {
+                effectsList.get(i).tick();
+                if (effectsList.get(i).isEnded()) {
+                    effectsList.get(i).end();
+                     effectsList.remove(i);
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
